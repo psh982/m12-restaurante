a56274f7760de5ac8222ed0ed7012640b9f0bd4c
100007050 <113420594+100007050@users.noreply.github.com>
2023-11-20 18:28:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://kit.fontawesome.com/9b3003b0ac.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Restaurante</title>
+</head>
+<body>
+    <section>
+        <div class="contenedor">
+            <div class="formulario">
+                <form action="POST">
+                    <h2>Iniciar Sesión</h2>
+
+                    <div class="input-contenedor">
+                        <i class="fa-solid fa-envelope"></i>
+                        <input type="email" required>
+                        <label for="#">Email</label>
+                    </div>
+
+                    <div class="input-contenedor">
+                        <i class="fa-solid fa-lock"></i>
+                        <input type="password" required>
+                        <label for="#">Contraseña</label>
+                    </div>
+
+                    <button>Iniciar Sesión</button>
+                </form>
+            </div>
+        </div>
+
+    </section>
+</body>
+</html>
